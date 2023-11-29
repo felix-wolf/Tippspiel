f693939be6d41bb8aaec780d4c117f21967a5c15
Felix Wolf <x3mdroid@gmail.com>
2023-11-29 10:29:58 +0100

    add first api endpoint in flask backend

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template
 from flask_login import *
+from datetime import datetime
 
 app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+
+@app.route('/time')
+def get_time():
+    return {'Time': "Hello"}
 
 
 @login_manager.user_loader
