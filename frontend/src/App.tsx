b4b1a0851254bb0ad3562b7668d8cbcdad4319c6
Felix Wolf <x3mdroid@gmail.com>
2023-11-30 17:23:19 +0100

    more yoinks

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,31 +3,41 @@ import {useEffect, useState} from "react";
 import logo from "../src/assets/images/logo.svg"
 
 export default function App() {
-  const [time, setTime] = useState(0)
+    const [time, setTime] = useState(0)
 
-  useEffect(() =>  {
-    fetch("/time").then((res) => res.json().then((data) => {
-          setTime(data["Time"])
-        })
+    useEffect(() =>  {
+            fetch("/time").then((res) => {
+                try {
+                    if (res.status == 200) {
+                        res.json().then((data) => {
+                            setTime(data["Time"])
+                        })
+                    }
+                }
+                catch (e) {
+                    console.log(e)
+                }
+            })
+        }
     )
-  })
-  return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload. This is a test.
-          </p>
-          <p>CurrentTime: {time}</p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-  );
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload. This is a test.
+                </p>
+                <p>CurrentTime: {time}</p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
