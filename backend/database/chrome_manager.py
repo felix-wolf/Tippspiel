8ae423266629f502734b72b2f0f8563400317a25
Felix Wolf <x3mdroid@gmail.com>
2023-12-16 14:57:26 +0100

    chrome unable to start

--- backend/database/chrome_manager.py
+++ backend/database/chrome_manager.py
@@ -6,14 +6,16 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 # configures the driver by getting the executable etc.
 def configure_driver():
+    print("configure driver")
     this_path = os.path.dirname(os.path.abspath(__file__))
     driver_dir = str(os.path.abspath(this_path + "/chromedriver"))
 
     # Start a new instance of the Chrome browser
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
+    print(driver_dir)
     if platform.system() == "Linux":
-        print("is linux")
+        print("is linux, ")
         driver = webdriver.Chrome(executable_path=driver_dir, options=options)
         return driver
     elif platform.system() == "Darwin":
