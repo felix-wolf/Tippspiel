a7a286c46dfb23772b9b9bcbe1e11b83085f76ab
Felix Wolf <x3mdroid@gmail.com>
2023-12-16 15:10:36 +0100

    small cleanup

--- backend/database/chrome_manager.py
+++ backend/database/chrome_manager.py
@@ -1,13 +1,11 @@
 from selenium import webdriver
 import os
 import platform
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
 
 # configures the driver by getting the executable etc.
 def configure_driver():
-    print("configure driver")
     this_path = os.path.dirname(os.path.abspath(__file__))
     driver_dir = str(os.path.abspath(this_path + "/chromedriver"))
 
@@ -15,9 +13,8 @@ def configure_driver():
     options = webdriver.ChromeOptions()
     service = Service(executable_path=driver_dir)
     options.add_argument('--headless')
-    print(driver_dir)
     if platform.system() == "Linux":
-        print("is linux, ")
+        print("is linux")
         driver = webdriver.Chrome(service=service, options=options)
         return driver
     elif platform.system() == "Darwin":
