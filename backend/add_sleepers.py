2c6fab3a39d213b0172a2a69baec42eb4d7a89c4
Felix Wolf <x3mdroid@gmail.com>
2024-01-07 12:34:34 +0100

    add user prompt before adding new bets

--- backend/add_sleepers.py
+++ backend/add_sleepers.py
@@ -1,5 +1,26 @@
 from models.bet import Bet
 from models.bet import Prediction
+from models.event import Event
+from models.user import User
+from models.athlete import Athlete
+from models.country import Country
+
+
+def getObjectName(object_type: str, object_id: str) -> str:
+    if object_type == "a":
+        return next(" ".join([a.first_name, a.last_name]) for a in Athlete.get_all() if a.id == object_id)
+    if object_type == "c":
+        return next(c.name for c in Country.get_all() if c.code == object_id)
+
+
+def confirm_with_user(user_id: str, event_id: str, objects: [], object_type: str) -> bool:
+    print("Are you sure you want to save this?")
+    print(f"Event: {Event.get_by_id(event_id).name}")
+    print(f"User: {User.get_by_id(user_id).name}")
+    for index, object_id in enumerate(objects):
+        print(f"Platz {index + 1}: {getObjectName(object_type, object_id)}")
+    return input("Type YES to confirm") == "YES"
+
 
 event_id = "535160c3433a9a4fc2d5890a42e16cc8"
 data = [
@@ -30,18 +51,26 @@ data = [
     },
 ]
 
-for entry in data:
-    user_id = entry['userId']
-    object_type = entry['type']
-    objects = entry['objects']
+if __name__ == '__main__':
+    for entry in data:
+        user_id = entry['userId']
+        object_type = entry['type']
+        objects = entry['objects']
 
-    predictions = []
+        predictions = []
 
-    new_bet = Bet(user_id, event_id)
+        new_bet = Bet(user_id, event_id)
 
-    for index, object_id in enumerate(objects):
-        predictions.append(Prediction(new_bet.id, object_id, "", index + 1))
-    new_bet.predictions = predictions
-    new_bet.save_to_db()
+        for index, object_id in enumerate(objects):
+            predictions.append(Prediction(new_bet.id, object_id, "", index + 1))
+        new_bet.predictions = predictions
+
+        if confirm_with_user(user_id, event_id, objects, object_type):
+            if new_bet.save_to_db():
+                print("saving successful.\n")
+            else:
+                print("Error when saving.\n")
+        else:
+            print("Did not save.\n")
 
 
