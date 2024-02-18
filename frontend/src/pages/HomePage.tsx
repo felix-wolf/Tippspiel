6a483041524dc6fb248960f9e4327ba074bc0b42
Felix Wolf <x3mdroid@gmail.com>
2024-02-18 17:47:50 +0100

    create general purpose caching logic

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,22 +2,23 @@ import { BettingGameList } from "../components/domain/lists/BettingGameList";
 import { useCurrentUser, useLogout } from "../models/user/UserContext";
 import { Button } from "../components/design/Button";
 import styles from "./HomePage.module.scss";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback } from "react";
 import { SiteRoutes, useNavigateParams } from "../../SiteRoutes";
 import { GamesContext } from "../contexts/GameContext";
 import { Game } from "../models/Game";
+import useFetch from "../useFetch";
+import { PulseLoader } from "react-spinners";
 
 export function HomePage() {
   const logout = useLogout();
   const navigate = useNavigateParams();
   const user = useCurrentUser();
-  const [games, setGames] = useState<Game[]>();
 
-  useEffect(() => {
-    Game.fetchAll().then((fetchedGames) => {
-      setGames(fetchedGames);
-    });
-  }, []);
+  const { loading, data } = useFetch({
+    fetchFunction: Game.fetchAll,
+    cache: { enabled: true, ttl: 60 * 2 },
+    key: "games",
+  });
 
   const logoutClick = useCallback(() => {
     logout().then();
@@ -27,10 +28,13 @@ export function HomePage() {
   return (
     <div className={styles.container}>
       <h3>Hallo {user?.name}!</h3>
-      <GamesContext.Provider value={games}>
-        <BettingGameList user={user} show_games={"user"} />
-        <BettingGameList user={user} show_games={"other"} />
-      </GamesContext.Provider>
+      {loading && <PulseLoader />}
+      {!loading && (
+        <GamesContext.Provider value={data}>
+          <BettingGameList user={user} show_games={"user"} />
+          <BettingGameList user={user} show_games={"other"} />
+        </GamesContext.Provider>
+      )}
       <Button onClick={logoutClick} title={"Logout"} type={"negative"} />
     </div>
   );
---
+++ frontend/src/useFetch.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import { useCache } from "./contexts/CacheContext";
+
+type useFetchProps = {
+  key: string;
+  initialEnabled?: boolean;
+  cache: {
+    enabled?: boolean;
+    ttl?: number;
+  };
+  fetchFunction: (args: any) => Promise<any>;
+  functionArgs?: any;
+};
+
+export default function useFetch<T = any>({
+  key,
+  initialEnabled = true,
+  cache,
+  fetchFunction,
+  functionArgs = undefined,
+}: useFetchProps) {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState<T | undefined>();
+  const [error, setError] = useState<any>();
+  const { getCache, setCache, deleteCache } = useCache();
+
+  const refetch = (hard: boolean = false) => {
+    setLoading(true);
+    setError(undefined);
+    if (cache?.enabled && getCache(key) !== undefined && !hard) {
+      setData(getCache(key));
+      setLoading(false);
+      setError(undefined);
+      return;
+    }
+    fetchFunction(functionArgs)
+      .then((data) => {
+        setData(data as T);
+        if (cache?.enabled) setCache(key, data, cache.ttl);
+      })
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  function inValidate(invalidationKey: string) {
+    deleteCache(invalidationKey);
+  }
+
+  useEffect(() => {
+    if (initialEnabled) refetch();
+  }, []);
+
+  return { loading, data, error, refetch, inValidate } as const;
+}
