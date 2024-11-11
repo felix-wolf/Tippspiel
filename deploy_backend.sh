99b40dce1cd20badde2fd8ddb754f3bfdd0940cb
Felix Wolf <x3mdroid@gmail.com>
2024-11-11 16:40:10 +0000

    change deploy_backend to start systemd service

--- deploy_backend.sh
+++ deploy_backend.sh
@@ -8,4 +8,4 @@ cd backend
 python3 -m pipenv install
 
 # run server in pipenv
-python3 -m pipenv run nohup gunicorn --bind 0.0.0.0:5000 'main:create_app(env="prod")' 2>&1 >~/gunicorn.log
+sudo systemctl start tippspiel_backend
