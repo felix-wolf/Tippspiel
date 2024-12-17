e32f1f9a5e47d7d5f94ff1a729a310c3adce020b
Felix Wolf <x3mdroid@gmail.com>
2024-12-17 23:10:42 +0100

    service worker

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -33,20 +33,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </CacheProvider>
   </React.StrictMode>,
 );
-
-// Register service worker
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((registration) => {
-        console.log(
-          "Service Worker registered with scope:",
-          registration.scope,
-        );
-      })
-      .catch((error) => {
-        console.error("Service Worker registration failed:", error);
-      });
-  });
-}
