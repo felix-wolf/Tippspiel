ec508183c2dc9c975655ed4a3ec82741ad967cde
Felix Wolf <x3mdroid@gmail.com>
2024-12-18 10:01:05 +0100

    2 sw files?

---
+++ frontend/firebase-messaging-sw.js
@@ -0,0 +1,16 @@
+self.addEventListener("install", (event) => {
+    console.log("Service worker installing...");
+  });
+  
+  self.addEventListener("activate", (event) => {
+    console.log("Service worker activating...");
+  });
+  
+  self.addEventListener("fetch", (event) => {
+    console.log("Fetching:", event.request.url);
+    // Optionally handle requests
+  });
+  
+  self.addEventListener('push', (event) => {
+    console.log('[Service Worker]: Received push event', event);
+  });
