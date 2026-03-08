f0ab920c8f853e7af22cb1087eac5018a2db5f42
Felix Wolf <felix.wolf@beyonnex.io>
2026-03-08 11:09:37 +0100

    fix(deployment): Fix backend deployment logic.

--- deploy_backend.sh
+++ deploy_backend.sh
@@ -1,3 +1,7 @@
+
+# stop systemd service
+sudo systemctl stop tippspiel_backend
+
 # pull latest
 git pull
 
@@ -7,11 +11,14 @@ cd backend
 # install deps
 uv sync
 
+# run backend tests before deploying
+uv run pytest -q || { echo "Tests failed. Aborting deploy."; exit 1; }
+
 # copy systemd service file
 sudo cp /home/felix/tippspiel/backend/tippspiel_backend.service /etc/systemd/system/
 
 # copy cron job file
 sudo cp /home/felix/tippspiel/backend/run_tippspiel_backend /etc/cron.d/
 
-# run server in pipenv
-sudo systemctl restart tippspiel_backend
+# start systemd service
+sudo systemctl start tippspiel_backend
